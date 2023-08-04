6b89fb1aa55661fb00b1b54ceac3b80165d810fb
Gaurav Singh <gsk162001@gmail.com>
2023-08-04 23:22:40 +0530

    readme updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,8 @@ import meal from '../assets/meal.mp4';
 import data from './data';
 
 export { images, meal, data };
+
+
+// let we want to import images  in some component :
+// import { images } from '../../constants';
+// to use an image ->  images.gallery01
